f5724a024b59de1f32671c2b765dea8709669c55
texlube <texlubedata@gmail.com>
2026-04-06 12:24:46 +0400

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -16,7 +16,7 @@ const categories = [
   { id: 'all-products', name: 'ALL PRODUCTS' },
   { id: 'passenger-car', name: 'PASSENGER CAR' },
   { id: 'truck-and-busses', name: 'TRUCKS & BUSSES' }, 
-  { id: 'motor-cycle', name: 'MOTOR CYCLE' },
+  { id: 'motorcycle', name: 'MOTORCYCLE' },
   { id: 'atf-and-gear', name: 'ATF & GEAR' },
   { id: 'industrial', name: 'INDUSTRIAL' },
   { id: 'hydraulic', name: 'HYDRAULIC' },
